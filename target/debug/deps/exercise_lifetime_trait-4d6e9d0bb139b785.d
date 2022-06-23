31fdb84a9c3027a755ff6552df585cebed9dbb67
tonyhoangtran92 <khaihoang2301@gmail.com>
2022-06-23 23:11:04 +0700

    commit

---
+++ target/debug/deps/exercise_lifetime_trait-4d6e9d0bb139b785.d
@@ -0,0 +1,5 @@
+d:\CNTT\Blockchain\Substrate_OCT_K3\Buoi_4\VBI-Rust-Workshop-3\exercise-lifetime-trait\target\debug\deps\exercise_lifetime_trait-4d6e9d0bb139b785.rmeta: src\main.rs
+
+d:\CNTT\Blockchain\Substrate_OCT_K3\Buoi_4\VBI-Rust-Workshop-3\exercise-lifetime-trait\target\debug\deps\exercise_lifetime_trait-4d6e9d0bb139b785.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/exercise_lifetime_trait-c040c276262a2d93.d
@@ -0,0 +1,5 @@
+d:\CNTT\Blockchain\Substrate_OCT_K3\Buoi_4\VBI-Rust-Workshop-3\exercise-lifetime-trait\target\debug\deps\exercise_lifetime_trait-c040c276262a2d93.rmeta: src\main.rs
+
+d:\CNTT\Blockchain\Substrate_OCT_K3\Buoi_4\VBI-Rust-Workshop-3\exercise-lifetime-trait\target\debug\deps\exercise_lifetime_trait-c040c276262a2d93.d: src\main.rs
+
+src\main.rs:
